9b0c3b3c7766e9ed7610ea2fa9f5514abfabbae5
Gabriel Baraboi <gabrielbaraboi5@gmail.com>
2019-12-24 23:35:47 +0200

    Created Dashboard, fixed some problems with login system

--- app/models.py
+++ app/models.py
@@ -1,9 +1,15 @@
 from datetime import datetime
-from flask_login import UserMixin
+from flask_login import UserMixin, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import login
 
 from app import db
+
+
+def is_admin():
+    if current_user.role == 'admin':
+        return True
+    return False
 
 
 class User(UserMixin, db.Model):
@@ -11,6 +17,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(128))
+    role = db.Column(db.String(), default='user')
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def set_password(self, password):
@@ -19,9 +26,6 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,9 +33,6 @@ class Post(db.Model):
     body = db.Column(db.String())
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __repr__(self):
-        return '<Post {}>'.format(self.body)
 
 
 @login.user_loader
--- app/routes.py
+++ app/routes.py
@@ -5,23 +5,34 @@ from werkzeug.urls import url_parse
 from app import app, db
 from app.forms import LoginForm
 from app.forms import RegisterForm
-from app.models import User
+from app.models import User, is_admin
 
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    return render_template('index.html', title='Home')
+    return render_template('index.html', title='Home', is_admin=is_admin())
+
+
+@app.route('/dashboard')
+def dashboard():
+    if not is_admin():
+        flash('You are not administrator', 'danger')
+        return redirect('index')
+    return render_template('dashboard/index.html', title='Dashboard')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        flash('You are already logged in', 'warning')
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
+            flash('Invalid username or password', 'danger')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
@@ -34,19 +45,22 @@ def login():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
+        flash('You are already logged in', 'warning')
         return redirect(url_for('index'))
     form = RegisterForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
+        user = User()
+        user.username = form.username.data
+        user.email = form.email.data
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
+        flash('Congratulations, you are now a registered user!', 'success')
+        return redirect('login')
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('login'))
